dc57e54be5b0df198b4ce8e1989acfccdebad79c
K. A. Mihashi Neelika Ranasinghe <mihashiranasinghe18@gmail.com>
2026-02-08 09:42:55 +0530

    Add Day2BadOOP good customer class

---
+++ src/Day2GoodOOP.java
@@ -0,0 +1,26 @@
+public class Day2GoodOOP {
+    class CustomerDataStorage {
+
+        public void saveToDatabase(Customer customer) {
+            // Code to save customer data to the database
+        }
+    }
+
+    class EmailSender {
+
+        public void sendEmailNotification(Customer customer) {
+            // Code to send email notification to the customer
+        }
+    }
+
+    class Customer {
+
+        private String name;
+        private String email;
+
+        public Customer(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+}
